0321a0aa597ff231dc12d140d281d9356060d359
cabri-schiissler <cabri.schiissler@ucalgary.ca>
2025-07-25 17:58:55 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(page_title="Happy 6 Months 💖", layout="centered")
+
+st.markdown("<h1 style='text-align: center; color: hotpink;'>💘 Happy 6 Month Anniversary! 💘</h1>", unsafe_allow_html=True)
+
+st.image([
+    "https://raw.githubusercontent.com/yourusername/first_gift/main/pic1.jpg",
+    "https://raw.githubusercontent.com/yourusername/first_gift/main/pic2.jpg",
+    "https://raw.githubusercontent.com/yourusername/first_gift/main/pic3.jpg"
+], width=250)
+
+st.markdown("""
+<div style='text-align: center; font-size: 20px; color: #c71585;'>
+I am so happy I met you!.<br>
+Thank you for everything<br>
+</div>
+""", unsafe_allow_html=True)
+
+st.markdown("### 🎵 Press play to hear our song")
+st.video("https://www.youtube.com/watch?v=Uc12NL0HN7w&list=RDUc12NL0HN7w&start_radio=1")
+
+
+if st.button("Click here 💝"):
+    st.snow()
+    st.success("I miss you. 💘")
